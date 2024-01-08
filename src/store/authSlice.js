2a8ca8fb942d5c323cc3bad07818d198ea065452
yashpatidar2207 <yashpatidar868@gmail.com>
2024-01-08 21:27:59 +0530

    create appwrite auth service, database,stoarage,file_upload and add redux_toolkit

---
+++ src/store/authSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    status : false,
+    userData: null
+}
+//          create reducers using the createSlice function
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            state.status = true;
+            state.userData = action.payload.userData;
+        },
+        logout: (state) => {
+            state.status = false;
+            state.userData = null;
+        }
+     }
+})
+
+export const {login, logout} = authSlice.actions;
+
+export default authSlice.reducer;
